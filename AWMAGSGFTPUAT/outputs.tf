d6e60e848001a57bf357a95b3e238249821c2626
lewmoore <lewis.moore2707@gmail.com>
2018-07-24 16:07:56 +0100

    Rename vpc output and change to id

--- AWMAGSGFTPUAT/outputs.tf
+++ AWMAGSGFTPUAT/outputs.tf
@@ -1,7 +1,7 @@
-output "vpc_name" {
-  value = "${aws_vpc.VPC1.name}"
+output "aws_vpc" {
+  value = "${aws_vpc.VPC1.id}"
 }
 
 output "security_group_name" {
-  value = "${aws_security_group.FTPUATSG.name}"
+  value = "${aws_security_group.FTPUATSG.id}"
 }
