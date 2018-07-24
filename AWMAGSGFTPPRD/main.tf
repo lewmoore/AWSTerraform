f20746fa6b2cbfa7d2877c997be9dbd8e432d663
lewmoore <lewis.moore2707@gmail.com>
2018-07-24 16:41:10 +0100

    Add windows vm to ftpprd with elastic ip and network interface

--- AWMAGSGFTPPRD/main.tf
+++ AWMAGSGFTPPRD/main.tf
@@ -22,13 +22,52 @@ resource "aws_subnet" "SNET12" {
   }
 }
 
-resource "aws_network_interface" "AZMAGNIC12" {
+resource "aws_network_interface" "AWMAGNIC12" {
   subnet_id = "${aws_subnet.SNET12.id}"
   private_ips = ["10.0.1.4"]
   security_groups = ["${data.terraform_remote_state.localstate.security_group_name}"]
 
   tags {
     Name = "AZMAGNIC12"
+    Resource_Group = "AWMAGRGFTPPRD"
+  }
+}
+
+resource "aws_eip" "FTPPRDEIP" {
+  instance = "${aws_instance.FTPPRDVM.id}"
+  vpc      = true
+  network_interface = "${aws_network_interface.AWMAGNIC12.id}"
+
+  tags {
+    Resource_Group = "AWMAGRGFTPPRD"
+  }
+}
+
+data "aws_ami" "amazon_windows_2016" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2016-English-Full-Base-2018.06.13"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+resource "aws_instance" "FTPPRDVM" {
+  ami           = "${data.aws_ami.amazon_windows_2016.id}"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = "${aws_network_interface.AWMAGNIC12.id}"
+    device_index = 0
+    }
+
+  tags {
     Resource_Group = "AWMAGRGFTPPRD"
   }
 }
