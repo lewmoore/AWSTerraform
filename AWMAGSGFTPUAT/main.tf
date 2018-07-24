101ca73a184c30fb6f0f557d0b08d14098543b15
lewmoore <lewis.moore2707@gmail.com>
2018-07-24 10:49:25 +0100

    Add ftpuat vpc with subnet

--- AWMAGSGFTPUAT/main.tf
+++ AWMAGSGFTPUAT/main.tf
@@ -3,3 +3,20 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region = "eu-west-2"
 }
+
+resource "aws_vpc" "VPC1" {
+  cidr_block = "10.0.0.0/23"
+
+  tags {
+    Name = "AWMAGSGFTPUAT"
+  }
+}
+
+resource "aws_subnet" "SNET11" {
+  vpc_id = "${aws_vpc.VPC1.id}"
+  cidr_block = "10.0.0.0/24"
+
+  tags {
+    Name = "AWMAGSGFTPUAT"
+  }
+}
