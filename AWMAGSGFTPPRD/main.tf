071d26a5ca07bf199c237c3c5a85774d4317f83b
lewmoore <lewis.moore2707@gmail.com>
2018-07-24 16:07:31 +0100

    Add local state and snet to vpc1

--- AWMAGSGFTPPRD/main.tf
+++ AWMAGSGFTPPRD/main.tf
@@ -11,3 +11,13 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region = "eu-west-2"
 }
+
+resource "aws_subnet" "SNET12" {
+  vpc_id = "${data.terraform_remote_state.localstate.aws_vpc}"
+  cidr_block = "10.0.1.0/24"
+
+  tags {
+    Name = "AWMAGSNET12"
+    Resource_Group = "AWMAGRGVPC1"
+  }
+}
