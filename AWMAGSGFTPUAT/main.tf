fabc4ea249cbe3b6efd01b5e9f80c9e1895a1d04
lewmoore <lewis.moore2707@gmail.com>
2018-07-24 15:19:37 +0100

    Add windows vm to ftpuat with elastic ip, and use existing resources

--- AWMAGSGFTPUAT/main.tf
+++ AWMAGSGFTPUAT/main.tf
@@ -22,3 +22,52 @@ resource "aws_subnet" "SNET11" {
     Resource_Group = "AWMAGRGVPC1"
   }
 }
+
+resource "aws_internet_gateway" "FTPUATIGW" {
+  vpc_id = "${aws_vpc.VPC1.id}"
+
+  tags {
+    Resource_Group = "AWMAGSGFTPUAT"
+  }
+}
+
+resource "aws_network_interface" "AWMAGNIC11" {
+  subnet_id = "${aws_subnet.SNET11.id}"
+  private_ips = ["10.0.0.4"]
+  security_groups = ["${aws_security_group.FTPUATSG.id}"]
+}
+
+resource "aws_eip" "FTPUATEIP" {
+  instance = "${aws_instance.FTPUATVM.id}"
+  vpc      = true
+  network_interface = "${aws_network_interface.AWMAGNIC11.id}"
+}
+
+data "aws_ami" "amazon_windows_2016" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2016-English-Full-Base-2018.06.13"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
+
+resource "aws_instance" "FTPUATVM" {
+  ami           = "${data.aws_ami.amazon_windows_2016.id}"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = "${aws_network_interface.AWMAGNIC11.id}"
+    device_index = 0
+    }
+
+  tags {
+    Resource_Group = "AWMAGSGFTPUAT"
+  }
+}
