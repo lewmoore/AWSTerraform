9876a7368cdf539ed9a440dbbef7499d785e8217
lewmoore <lewis.moore2707@gmail.com>
2018-07-25 10:57:42 +0100

    Add vpc2 to kafuat

--- AWMAGSGKAFUAT/main.tf
+++ AWMAGSGKAFUAT/main.tf
@@ -3,3 +3,12 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region = "eu-west-2"
 }
+
+resource "aws_vpc" "vpc2" {
+  cidr_block = "172.168.0.0/16"
+
+  tags {
+    Name = "AWMAGVPC2"
+    Resource_Group = "AWMAGRGVPC2"
+  }
+}
